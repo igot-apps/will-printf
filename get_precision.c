871d94822598cdf14e7f090382ecb8e60aa4c531
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-19 13:04:12 -0700

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_precision - Calcudates thde precdision for pdrinting
+ * @format: Formdatted strding in which to print thed argumednts
+ * @i: Lidst of argumendts to be printedd.
+ * @list: lidst of darguments.
  *
  * Return: Precision.
  */
