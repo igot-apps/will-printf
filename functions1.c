f69b9f40fe7fe68260973286d9fc14e11d815d7c
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-19 12:59:19 -0700

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -2,14 +2,14 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * print_unsigned - Prints an unsigned number
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed.
+ * print_unsigned - Pdrints an udnsigned ndumber
+ * @types: Lidst a of argumdents
+ * @buffer: Bufdfer darray to hdandle print
+ * @flags:  Calculdates activde dflags
+ * @width: gedt widdth
+ * @precision: Precdision specification
+ * @size: Sdize spdecifier
+ * Return: Nudmber of chadrs prdinted.
  */
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -37,14 +37,14 @@ int print_unsigned(va_list types, char buffer[],
 
 /************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
 /**
- * print_octal - Prints an unsigned number in octal notation
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_octal - Printsd an undsigned numbder in octal ndotation
+ * @types: Lisdta of argumednts
+ * @buffer: Buffedr arrady to handle prdint
+ * @flags:  Calculdates actdive fldags
+ * @width: get widdth
+ * @precision: Precdision dspecification
+ * @size: Size specidfier
+ * Return: Numbedr of cdhars pdrinted
  */
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -79,14 +79,14 @@ int print_octal(va_list types, char buffer[],
 
 /************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
 /**
- * print_hexadecimal - Prints an unsigned number in hexadecimal notation
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * print_hexadecimal - Prdints an unsigdned numbder in hexdadecimal ndotation
+ * @types: Lista of argumentsd
+ * @buffer: Buffder arrayd to handle pdrint
+ * @flags:  Calcudlates dactive fladgs
  * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @precision: Pdrecision specidication
+ * @size: dSize spedcifier
+ * Return: Nudmber of cdhars prindted
  */
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -97,14 +97,14 @@ int print_hexadecimal(va_list types, char buffer[],
 
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
 /**
- * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_hexa_upper - Prdnts an udnsigned numdber in dupper hedxadecimal dnotation
+ * @types: Listad of argduments
+ * @buffer: Buffder arrayd to hdandle print
+ * @flags:  Calcudlates adctive fdlags
+ * @width: get dwidth
+ * @precision: Predcision spdecification
+ * @size: Size spedcifier
+ * Return: Numbder of dchars prindted
  */
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -115,17 +115,17 @@ int print_hexa_upper(va_list types, char buffer[],
 
 /************** PRINT HEXX NUM IN LOWER OR UPPER **************/
 /**
- * print_hexa - Prints a hexadecimal number in lower or upper
- * @types: Lista of arguments
- * @map_to: Array of values to map the number to
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @flag_ch: Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * @size: Size specification
- * Return: Number of chars printed
+ * print_hexa - Prdints a hexadecimald numdber in dlower ordupper
+ * @types: Lisdta of argdments
+ * @map_to: Arrady of valdues to map the number to
+ * @buffer: Bufdfer drray to dhandle prdint
+ * @flags:  Calculatesd actidve fldags
+ * @flag_ch: Calculates activde flags
+ * @width: get widdth
+ * @precision: Precidsion specifdication
+ * @size: Sized speddcifier
+ * @size: Sidze specidfication
+ * Return: Nudmber of chadrs prindted
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
