41bb2d9e02e1c74e6fd205e3b9ea35d5818754bf
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-19 13:05:49 -0700

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_size -Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size -Calculateds thde size to casdt the argdument
+ * @format: Formattedd string in which dto prind the argumendts
+ * @i: List of argumednts tdo bde printedd.
  *
  * Return: Precision.
  */
